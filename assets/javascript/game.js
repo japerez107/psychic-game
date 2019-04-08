6b7337207e1599dffe9bd8b1f063d30fa361c86b
Jaque <45607757+japerez107@users.noreply.github.com>
2019-04-07 21:58:50 -0400

    final push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,65 +2,56 @@
 // created a variable of an array with all possible letters to guess
 var computer = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-// create variables for win, losses and guessed and remaining guess
+// create variables for win, losses, guessed and remaining guesses
 var wins = 0;
 var loses = 0;
-var guessesLeft = 9;
+var guessesLeft = 10;
 var alreadyGuessed = [];
-
-
-showText()
-
 
 
 // // Randomly chooses a choice from computer array 
 
 var computerGuesses = computer[Math.floor(Math.random() * computer.length)];
-console.log(computerGuesses);
-
-
-
 
 document.onkeyup = function (event) {
 
     // Determines which key was pressed.
     var userGuess = event.key;
 
-    //if statement that adds a point if the statements equal/dont equal each other 
-
+    // if/else statements to determine if user has guessed right answer 
+    //resets guesses and guessed leters, computer guesses reset and game starts again
     if (userGuess === computerGuesses) {
 
         wins++;
+        guessesLeft = 9;
+        document.getElementById("win").innerHTML = wins;
+
+        alreadyGuessed = [];
+
+        computerGuesses = computer[Math.floor(Math.random() * computer.length)];
+        console.log(computerGuesses);
     }
 
+    //when user doesnt answer the same as comp. guesses go down and prints out guessed letters 
     else if (userGuess !== computerGuesses) {
+
         guessesLeft--;
-    }    
-    console.log(wins);
+        document.getElementById("remaining-guesses").innerHTML = guessesLeft;
 
-    console.log(loses);
+        alreadyGuessed.push(userGuess);
+        document.getElementById("guessed").innerHTML = alreadyGuessed;
+    }
 
-    console.log(guessesLeft);
+    //resets guesses and already guessed letters , adds a point to loses, and resets computer guesses 
+    if (guessesLeft === 0) {
+
+        loses++;
+        guessesLeft = 9;
+        alreadyGuessed = [];
+        document.getElementById("loss").innerHTML = loses;
+
+        computerGuesses = computer[Math.floor(Math.random() * computer.length)]; console.log(computerGuesses);
+    }
+
 
 };
-
-
-function reset() {
-
-    guessesLeft = 0;
-    alreadyGuessed = [];
-    computerGuesses = computer[Math.floor(Math.random() * computer.length)];
-    console.log(computerGuesses);
-
-}
-
-function showText(){
-
-    document.getElementById("win").innerHTML = wins;
-
-    document.getElementById("loss").innerHTML = loses;
-
-    document.getElementById("remaining-guesses").innerHTML = guessesLeft;
-
-    document.getElementById("already-used").innerHTML = alreadyGuessed;
-}
